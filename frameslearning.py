ac45a974e0cfc8b97074261b08ff8d87fb5889f1
aarav245 <aaravmisra24@gmail.com>
2025-08-03 19:26:29 -0700

    Add files via upload

---
+++ frameslearning.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+screen = Tk()
+screen.geometry("400x400")
+screen.title("ice cream toppings")
+label1 = Label(screen,text = "Choose your flavors",font = ("Times New Roman",30))
+label1.pack()
+screen.mainloop()
